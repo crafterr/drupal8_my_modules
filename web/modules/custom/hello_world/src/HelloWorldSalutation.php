9b540344d384246037424aa2b76a2e02837013b7
crafter <adam@crafter.net.pl>
2019-09-02 22:54:40 +0200

    service

--- web/modules/custom/hello_world/src/HelloWorldSalutation.php
+++ web/modules/custom/hello_world/src/HelloWorldSalutation.php
@@ -4,6 +4,7 @@
 namespace Drupal\hello_world;
 
 use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Component\Datetime\Time;
@@ -12,15 +13,26 @@ class HelloWorldSalutation implements HelloWorldSalutationInterface{
   use StringTranslationTrait;
 
   /**
-   * @var \Drupal\Component\Datetime\TimeInterface
+   * @var TimeInterface
    */
   private $time;
 
-  public function __construct(TimeInterface $time) {
+  /**
+   * @var ConfigFactoryInterface
+   */
+  private $configFactory;
+
+  public function __construct(TimeInterface $time, ConfigFactoryInterface $configFactory) {
     $this->time = $time;
+    $this->configFactory = $configFactory;
   }
 
   public function getSalutation() {
+    $config = $this->configFactory->get('hello_world.custom_salutation');
+    $salutation = $config->get('salutation');
+    if ($salutation != "") {
+      return $salutation;
+    }
     $time = new DrupalDateTime();
     if ((int) $time->format('G') >= 00 && (int) $time->format('G') < 12) {
       return $this->t('Good morning world');
