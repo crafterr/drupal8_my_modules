0da5c7e983b01bceecf8465e15c151ef5fd4d0ce
crafter <adam@crafter.net.pl>
2019-10-04 14:34:46 +0200

    working with link

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\hello_world\Controller;
 
+use Drupal\Console\Bootstrap\Drupal;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\hello_world\HelloWorldSalutationInterface;
 use Drupal\node\NodeInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -64,5 +67,33 @@ class HelloWorldController extends ControllerBase {
       '#type' => 'markup',
       '#markup' => $this->salutation->getSalutation()
     ];
+  }
+
+  /**
+   *
+   */
+  public function urlSample() {
+    //url object
+    $url = Url::fromRoute('hello_world.hello.url');
+
+    //get link from url object
+    /**
+     * @var \Drupal\Core\Utility\LinkGeneratorInterface $linkGeneratorService
+     */
+    $linkGeneratorService = \Drupal::service('link_generator');
+    $link = $linkGeneratorService->generate('My link',$url);
+
+    //or faster
+    $link = Link::fromTextAndUrl('My link',$url);
+
+
+    //the same way
+    $link = $link->toString();
+    //or
+    $link = $linkGeneratorService->generateFromLink($link);
+
+    echo $link; die();
+    return [];
+
   }
 }
