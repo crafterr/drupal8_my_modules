3cea61ff16cb7066976104632836a609419c8d75
crafter <adam@crafter.net.pl>
2019-10-21 22:20:11 +0200

    test

--- web/modules/custom/my_article_view/library/js/library.js
+++ web/modules/custom/my_article_view/library/js/library.js
@@ -1,14 +1,22 @@
 (function ($, Drupal) {
 Drupal.behaviors.myBehavior = {
   attach: function (context, settings) {
-   // $('.drupal-page', context).once().each(function() {
-     $('a.clicker',context).on('click',function() {
+    //$('.drupal-page').once().each(function() {
+     //$('a.clicker',context).on('click',function() {
        //console.log(context);
        var h2 = $('h2.node__title',context);
-       console.log(settings);
+       //console.log('adam ma kota');
        //console.log(h2.text());
-     });
-   // });
+        $('a.clicker').once().on('click',function(){
+           console.log('weszlo');
+        });
+        $('select.myselect').once().on('change',function(){
+           console.log($(this).val());
+        });
+
+
+    // });
+    //});
 
   }
 }
