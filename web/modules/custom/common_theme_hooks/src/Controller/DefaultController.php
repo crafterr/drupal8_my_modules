6ce27449a5487ddbfaece731df39811b8838873f
crafter <adam@crafter.net.pl>
2019-11-03 00:14:37 +0100

    common theme hooks

--- web/modules/custom/common_theme_hooks/src/Controller/DefaultController.php
+++ web/modules/custom/common_theme_hooks/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Drupal\common_theme_hooks\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 
 /**
  * Class DefaultController.
@@ -18,6 +19,39 @@ class DefaultController extends ControllerBase {
       '#theme' => 'item_list',
       '#items' => $items,
       '#list_type' => 'ol'
+    ];
+  }
+
+  public function links() {
+    $links = [
+      [
+        'title' => 'Link 1',
+        'url' => Url::fromRoute('common_theme_hooks.default_controller_links'),
+      ],
+      [
+        'title' => 'Link 1',
+        'url' => Url::fromRoute('common_theme_hooks.default_controller_hello'),
+      ]
+    ];
+
+    return [
+      '#theme' => 'links',
+      '#links' => $links,
+      '#set_active_class' => true,
+    ];
+  }
+
+  public function table() {
+    $header = ['Column 1', 'Column 2'];
+    $rows = [
+      ['Row 1, Column 1', 'Row 1, Column 2'],
+      ['Row 2, Column 1', 'Row 2, Column 2']
+    ];
+
+    return [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
     ];
   }
 
